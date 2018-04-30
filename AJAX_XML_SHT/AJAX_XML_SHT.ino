3eb5c04aadb6ee7c69ad71fc77c0f9a506d3208e
theiothing <34738514+theiothing@users.noreply.github.com>
2018-04-30 10:19:07 +0200

    Add files via upload

---
+++ AJAX_XML_SHT/AJAX_XML_SHT.ino
@@ -0,0 +1,59 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+ESP8266WebServer server(80);
+#include <WEMOS_SHT3X.h>
+SHT3X sht30(0x45);
+#include "init.h"
+#include "web_page.h"
+
+float temp;
+float hum;
+
+void XMLcontent()
+{
+  String XML = String(XML_Template); // copy template to String for to be able to modify dynamic values
+  XML.replace("#value_001#", String(hum)+ "%");
+  XML.replace("#value_002#", String(temp) + "°C");
+  server.send(200, "text/xml", XML);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(500);
+  Serial.println();
+  Serial.println("let's begin");
+  WiFi.begin(ssid,password);
+  while(WiFi.status()!=WL_CONNECTED){
+    Serial.print(".");
+    delay(500);}
+  WiFi.mode(WIFI_STA);
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("Station IP address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", WebsiteContent);
+  server.on("/xml", XMLcontent);
+  server.begin();
+}
+
+void loop()
+{
+  if(sht30.get()==0){
+    Serial.print("Temperature in Celsius : ");
+    temp = sht30.cTemp;
+    Serial.println(temp);
+    hum= sht30.humidity;
+    Serial.print("Relative Humidity : ");
+    Serial.println(hum);
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("Error!");
+
+  }
+  server.handleClient();
+
+}
+
---
+++ AJAX_XML_SHT/web_page.h
@@ -0,0 +1,102 @@
+static const char WebsiteTemplate[] PROGMEM = // #xxxxxxxx# are later to be changed in c function WebsiteContent()
+R"=====(
+  <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <style>
+
+  body{
+    background-color: #373c44;
+    color: #ccc;
+    font-family: Nexa, sans-serif;
+  }
+  span{
+    font-size: 80%;
+  }
+  p{
+    font-size: 150%;
+    padding: 0;
+    margin-bottom: 5px;
+  }
+  .col-container {
+    position: absolute;
+    top:0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    display: table;
+    width: 100%;
+  }
+  .col {
+    margin:3px;
+    text-align: center;
+    display: table-cell;
+    padding: 20px;
+    background-color: #282a2d;
+  }
+</style>
+</head>
+<body onload='xml_process()'>
+  <div class="col-container" >
+    <div class="col">
+      <p id="div_001">#value_001#</p>
+      <span class="measure">rel. humidity</span>
+    </div>
+    <div class="col">
+      <p id="div_002">#value_002#</p>
+      <span class="measure">temperature</span>
+    </div>
+  </div>  
+
+  <SCRIPT> // here starts the Java script for AJAX method
+
+    var xmlHttp = new XMLHttpRequest(); // for IE7+, Firefox, Chrome, Opera, Safari (older IE not supported)
+
+    function response()
+      {
+        if(xmlHttp.readyState==4 && xmlHttp.status==200)
+        {
+          xmlResponse = xmlHttp.responseXML;
+          xmldoc = xmlResponse.getElementsByTagName('xml_tag_001');
+          message = xmldoc[0].firstChild.nodeValue;
+          document.getElementById('div_001').innerHTML=message;
+          xmldoc = xmlResponse.getElementsByTagName('xml_tag_002');
+          message = xmldoc[0].firstChild.nodeValue;
+          document.getElementById('div_002').innerHTML=message;         
+        }
+      }
+
+    function xml_process()
+    {
+      if(xmlHttp.readyState==0 || xmlHttp.readyState==4)
+      {
+        xmlHttp.open('PUT', 'xml', true); // execute c function XMLcontent()
+        xmlHttp.onreadystatechange = response;
+        xmlHttp.send(null); //Request with no data in request body
+      }
+      setTimeout('xml_process()', 1000);
+    }
+
+  </SCRIPT>
+  </body>
+</html>
+)=====";
+
+
+const char XML_Template[] PROGMEM = // #xxxxxxxx# are later to be changed in c function XML_Content()
+R"=====(<?xml version='1.0'?>
+  <Variables>
+    <xml_tag_001>#value_001#</xml_tag_001>
+    <xml_tag_002>#value_002#</xml_tag_002>
+  </Variables>
+)=====";
+
+
+void WebsiteContent()
+{
+  server.send(200,"text/html", WebsiteTemplate);
+}
+
+
